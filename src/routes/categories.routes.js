6da1258d85493770d5c7348c8bcf12a3772bd8f8
Matthew Tuson <mtuson1414@gmail.com>
2025-03-13 22:16:03 +1300

    refactor categories data structure to use objects with ids for improved management

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const categories = [
-  "Food",
-  "Transportation",
-  "Housing",
-  "Utilities",
-  "Insurance",
-  "Healthcare",
-  "Education",
-  "Entertainment",
-  "Clothing",
-  "Miscellaneous",
+  { id: 1, name: "Food" },
+  { id: 2, name: "Transportation" },
+  { id: 3, name: "Housing" },
+  { id: 4, name: "Utilities" },
+  { id: 5, name: "Insurance" },
+  { id: 6, name: "Healthcare" },
+  { id: 7, name: "Education" },
+  { id: 8, name: "Entertainment" },
+  { id: 9, name: "Clothing" },
+  { id: 10, name: "Miscellaneous" },
 ];
 
 router.get("/", async (req, res) => {

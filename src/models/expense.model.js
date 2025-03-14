caf23f4b281f0ec89d2ff5c947f29f223f8bbaef
Matthew Tuson <mtuson1414@gmail.com>
2025-03-14 17:26:02 +1300

    update expense model to change category type from string to number

--- src/models/expense.model.js
+++ src/models/expense.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ExpenseSchema = new mongoose.Schema({
   description: { type: String, required: true },
   amount: { type: Number, required: true },
-  category: { type: String, required: true },
+  category: { type: Number, required: true },
   currency: { type: String, required: true },
   date: { type: Date, default: Date.now },
   trip: { type: mongoose.Schema.Types.ObjectId, ref: "Event" }, 

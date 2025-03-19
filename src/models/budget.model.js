22b5c09555665b95b474b53d770f96170b3b6266
Matthew Tuson <mtuson1414@gmail.com>
2025-03-19 21:16:53 +1300

    refactor budget model: rename amount to total and add amountTracking field

--- src/models/budget.model.js
+++ src/models/budget.model.js
@@ -3,18 +3,19 @@ const mongoose = require("mongoose");
 const BudgetSchema = new mongoose.Schema({
   name: { type: String, required: true },
   event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
-  amount: { type: Number, required: true },
+  total: { type: Number, required: true },
+  amountTracking: { type: Number },
   currency: { type: String, required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   categoryBudgets: [
     {
       category: String,
-      allocatedAmount: Number
-    }
+      allocatedAmount: Number,
+    },
   ],
   expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
-  isActive: { type: Boolean, default: false }
+  isActive: { type: Boolean, default: false },
 });
 
 const Budget = mongoose.model("Budget", BudgetSchema);

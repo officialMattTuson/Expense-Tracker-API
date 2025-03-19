f8d29d467fabfa3b5c15c235acab4d593478c5f8
Matthew Tuson <mtuson1414@gmail.com>
2025-03-19 21:17:44 +1300

    refactor budget model: rename total to amount and adjust schema formatting

--- src/models/budget.model.js
+++ src/models/budget.model.js
@@ -3,19 +3,18 @@ const mongoose = require("mongoose");
 const BudgetSchema = new mongoose.Schema({
   name: { type: String, required: true },
   event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
-  total: { type: Number, required: true },
-  amountTracking: { type: Number },
+  amount: { type: Number, required: true },
   currency: { type: String, required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   categoryBudgets: [
     {
       category: String,
-      allocatedAmount: Number,
-    },
+      allocatedAmount: Number
+    }
   ],
   expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
-  isActive: { type: Boolean, default: false },
+  isActive: { type: Boolean, default: false }
 });
 
 const Budget = mongoose.model("Budget", BudgetSchema);

8721464222a72920ac10bab5ad538b22dae4ffd7
Matthew Tuson <mtuson1414@gmail.com>
2025-04-01 20:09:37 +1300

    update expense model to include location details

--- src/models/expense.model.js
+++ src/models/expense.model.js
@@ -6,8 +6,19 @@ const ExpenseSchema = new mongoose.Schema({
   category: { type: Number, required: true },
   currency: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  trip: { type: mongoose.Schema.Types.ObjectId, ref: "Event" }, 
-  budget: { type: mongoose.Schema.Types.ObjectId, ref: "Budget" }
+  trip: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+  budget: { type: mongoose.Schema.Types.ObjectId, ref: "Budget" },
+  location: {
+    name: { type: String },
+    address: { type: String },
+    city: { type: String },
+    country: { type: String },
+    coordinates: {
+      lat: { type: Number, min: -90, max: 90 },
+      lng: { type: Number, min: -180, max: 180 },
+    },
+  },
 });
+
 const Expense = mongoose.model("Expense", ExpenseSchema);
 module.exports = Expense;

633d5096090968e075323e26931c9f3128193596
Matthew Tuson <mtuson1414@gmail.com>
2025-06-23 23:13:43 +1200

    update trip model, no longer links to expenses, instead links through budget

--- src/models/trip.model.js
+++ src/models/trip.model.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const TripSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
+  destinations: [{type: String, required: true}],
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   budgets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Budget" }], 
-  expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
 });
 
 const Trip = mongoose.model("Trip", TripSchema);

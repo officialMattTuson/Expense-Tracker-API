40416b2dce2036174dd7404a63183ee89dd1c872
Matthew Tuson <mtuson1414@gmail.com>
2025-06-25 22:20:19 +1200

    update list of categories

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -3,15 +3,10 @@ const router = express.Router();
 
 const categories = [
   { id: 1, name: "Food" },
-  { id: 2, name: "Transportation" },
-  { id: 3, name: "Housing" },
-  { id: 4, name: "Utilities" },
-  { id: 5, name: "Insurance" },
-  { id: 6, name: "Healthcare" },
-  { id: 7, name: "Education" },
-  { id: 8, name: "Entertainment" },
-  { id: 9, name: "Clothing" },
-  { id: 10, name: "Miscellaneous" },
+  { id: 2, name: "Transport" },
+  { id: 3, name: "Accommodation" },
+  { id: 4, name: "Shopping" },
+  { id: 5, name: "Activities" },
 ];
 
 router.get("/", async (req, res) => {
